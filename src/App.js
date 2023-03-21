36200ceb05835bd48d96e6a574a9e574c8ee6359
Willian <wp.uenp@gmail.com>
2023-03-21 19:54:19 -0300

    Seção ultimos lançamentos e card saiba mais, usando props

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Header from './components/Header'
 import Pesquisa from './components/Pesquisa'
+import UltimosLancamentos from './components/UltimosLancamentos'
 import styled from 'styled-components';
 
 const AppContainer = styled.div`
@@ -13,6 +14,7 @@ function App() {
     <AppContainer>
       <Header />
       <Pesquisa />
+      <UltimosLancamentos />
     </AppContainer>
   );
 }
